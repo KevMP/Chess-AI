9ab48df2c2791de39b8392c12c4015569cce9740
KevMP <kevmp63@gmail.com>
2023-03-18 18:26:03 -0400

    test

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -8,6 +8,7 @@ int main()
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
+
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
