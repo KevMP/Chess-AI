7d87fcaaa5f95d2db84cb834ed4e4b199a2ded74
KevMP <109618100+KevMP@users.noreply.github.com>
2023-03-18 21:38:39 -0400

    Created Chess namespace

---
+++ Project/Project/ChessPieces.h
@@ -0,0 +1,9 @@
+#ifndef CHESS_PIECES_H
+#define CHESS_PIECES_H
+
+namespace chess {
+    int test = 1;
+
+}
+
+#endif // CHESS_PIECES_H
--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,17 +1,14 @@
 #include  <SFML/Graphics.hpp>
+#include "ChessPieces.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Chess");
     sf::Event event;
 
     while (window.isOpen()) {
-
         while (window.pollEvent(event)) {
-
             if (event.type == sf::Event::Closed) {
-
                 window.close();
             }
         }
